b367a244887c11b871a791c7e3813b39f2dbfb80
a-faraz <anjumfaraz10@gmail.com>
2017-01-04 09:46:13 -0600

    Project Skeleton

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+
+
+class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {};
+	}
+
+	render() {
+
+		return (
+			<div>	
+				<div className="page-title"> Home Movie Collection </div>
+
+			</div>
+		)
+	}
+}
+
+ReactDOM.render( <App />  , document.querySelector('.container'));
